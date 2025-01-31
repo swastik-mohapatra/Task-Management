7ab4042f4d129be4ee15a2a69c3e5e1ca7a9a5e1
Swastik Mohapatra <swastikmohapatra@Swastiks-MacBook-Air.local>
2025-02-01 01:31:56 +0530

    board

--- src/components/FilterCreateTasks.tsx
+++ src/components/FilterCreateTasks.tsx
@@ -1,9 +1,108 @@
-import React from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useState } from "react";
+import { CiSearch } from "react-icons/ci";
 
 const FilterCreateTasks = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
     <>
-      <div className="text-sm">Filter By:</div>
+      <div className="flex justify-between items-center">
+        <div className="flex gap-4">
+          <div>Filter By: </div>
+          <Box className="flex gap-2">
+            <FormControl sx={{ width: "auto" }}>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                onChange={handleChange}
+                size="small"
+                displayEmpty
+                sx={{
+                  borderRadius: "16px",
+                  paddingX: "8px",
+                  minWidth: "80px",
+                  fontSize: "14px",
+                  height: "28px",
+                  "& .MuiSelect-select": {
+                    display: "flex",
+                    alignItems: "center",
+                    paddingY: "4px",
+                  },
+                }}
+              >
+                <MenuItem value="" disabled>
+                  Category
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl sx={{ width: "auto" }}>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                onChange={handleChange}
+                size="small"
+                displayEmpty
+                sx={{
+                  borderRadius: "16px",
+                  paddingX: "8px",
+                  minWidth: "80px",
+                  fontSize: "14px",
+                  height: "28px",
+                  "& .MuiSelect-select": {
+                    display: "flex",
+                    alignItems: "center",
+                    paddingY: "4px",
+                  },
+                }}
+              >
+                <MenuItem value="" disabled>
+                  Due Date
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+        <div className="flex gap-4">
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-2 pointer-events-none">
+              <CiSearch />
+            </div>
+            <input
+              type="search"
+              className="block p-2 ps-8 text-sm text-gray-900 border border-gray-300 rounded-2xl bg-gray-50 dark:border-gray-600 dark:text-white placeholder-gray-700"
+              placeholder="Search"
+              required
+            />
+          </div>
+
+          <Button
+            variant="contained"
+            sx={{ backgroundColor: "#7B1984", borderRadius: "22px", paddingX:"29px" }}
+          >
+            ADD TASKS
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/TabScreen/ListView.tsx
+++ src/components/TabScreen/ListView.tsx
@@ -1,11 +1,83 @@
-import React from "react";
+import { MdExpandMore } from "react-icons/md";
 import FilterCreateTasks from "../FilterCreateTasks";
+import {
+  Accordion,
+  AccordionActions,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Divider,
+  Typography,
+} from "@mui/material";
+import { FiPlus } from "react-icons/fi";
 
 const ListView = () => {
   return (
     <>
-      <FilterCreateTasks />
-      <div className="mt-6">ListView</div>
+      <div className="mt-2">
+        <FilterCreateTasks />
+        <div className="mt-6">
+          <Divider />
+          <div className="mt-4">
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<MdExpandMore />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+                sx={{ backgroundColor: "#FAC3FF" }}
+              >
+                <Typography component="span">Todo</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{ height: "350px", backgroundColor: "#FFFAEA" }}
+              >
+                <Box>
+                  <Button variant="text" startIcon={<FiPlus />} sx={{ color: "#000000",textTransform:"capitalize" }}>
+                    Add Task
+                  </Button>
+                  <Divider/>
+                </Box>
+                No Tasks in To-Do
+              </AccordionDetails>
+            </Accordion>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<MdExpandMore />}
+                aria-controls="panel2-content"
+                id="panel2-header"
+                sx={{ backgroundColor: "#85D9F1" }}
+              >
+                <Typography component="span">In-Progress</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{ height: "350px", backgroundColor: "#FFFAEA" }}
+              >
+                No Tasks in Progress
+              </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+              <AccordionSummary
+                expandIcon={<MdExpandMore />}
+                aria-controls="panel3-content"
+                id="panel3-header"
+                sx={{ backgroundColor: "#CEFFCC" }}
+              >
+                <Typography component="span">Completed</Typography>
+              </AccordionSummary>
+              <AccordionDetails
+                sx={{ height: "350px", backgroundColor: "#FFFAEA" }}
+              >
+                No Tasks in Completed
+              </AccordionDetails>
+              <AccordionActions>
+                <Button>Cancel</Button>
+                <Button>Agree</Button>
+              </AccordionActions>
+            </Accordion>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
