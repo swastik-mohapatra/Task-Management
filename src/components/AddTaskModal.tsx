d7b051b90f778deaca20c901297f96c7b1fa7016
Swastik Mohapatra <swastik.mohapatra@incture.com>
2025-02-07 19:18:52 +0530

    dialog message

--- src/components/AddTaskModal.tsx
+++ src/components/AddTaskModal.tsx
@@ -4,8 +4,12 @@ import {
   Button,
   Divider,
   FormControl,
+  FormControlLabel,
+  FormLabel,
   InputLabel,
   MenuItem,
+  Radio,
+  RadioGroup,
   Select,
   styled,
   TextField,
@@ -48,7 +52,8 @@ const AddTaskModal = ({
   setOpenAddModal,
 }: AddTaskModalProps) => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const [logActivities, setLogActivities] = useState<LogEntry[]>([]); 
+  const [logActivities, setLogActivities] = useState<LogEntry[]>([]);
+  const [selectedOption, setSelectedOption] = useState("details");
   const taskDetail = useSelector(
     (state: any) => state?.systemConfigReducer?.taskDetails
   );
@@ -93,7 +98,7 @@ const AddTaskModal = ({
         description,
       };
       await addDoc(logCollectionRef, newLog);
-      setLogActivities((prevLogs) => [newLog, ...prevLogs]); 
+      setLogActivities((prevLogs) => [newLog, ...prevLogs]);
     } catch (error) {
       console.error("Error adding log activity:", error);
     }
@@ -168,7 +173,7 @@ const AddTaskModal = ({
       };
 
       const taskRef = await addDoc(taskCollectionRef, formattedTaskDetail);
-      await addLogActivity(taskRef.id, "Task Created"); 
+      await addLogActivity(taskRef.id, "Task Created");
       getTaskData(dispatch);
       dispatch(setAccessData({ type: "taskDetails", response: {} }));
       setSelectedFiles([]);
@@ -204,19 +209,29 @@ const AddTaskModal = ({
 
       const changes = [];
       if (updatePayload.taskName !== currentTaskData?.taskName) {
-        changes.push(`Task Name: "${currentTaskData?.taskName}" → "${updatePayload.taskName}"`);
+        changes.push(
+          `Task Name: "${currentTaskData?.taskName}" → "${updatePayload.taskName}"`
+        );
       }
       if (updatePayload.description !== currentTaskData?.description) {
-        changes.push(`Description: "${currentTaskData?.description}" → "${updatePayload.description}"`);
+        changes.push(
+          `Description: "${currentTaskData?.description}" → "${updatePayload.description}"`
+        );
       }
       if (updatePayload.category !== currentTaskData?.category) {
-        changes.push(`Category: "${currentTaskData?.category}" → "${updatePayload.category}"`);
+        changes.push(
+          `Category: "${currentTaskData?.category}" → "${updatePayload.category}"`
+        );
       }
       if (updatePayload.dueDate !== currentTaskData?.dueDate) {
-        changes.push(`Due Date: "${currentTaskData?.dueDate}" → "${updatePayload.dueDate}"`);
+        changes.push(
+          `Due Date: "${currentTaskData?.dueDate}" → "${updatePayload.dueDate}"`
+        );
       }
       if (updatePayload.status !== currentTaskData?.status) {
-        changes.push(`Status: "${currentTaskData?.status}" → "${updatePayload.status}"`);
+        changes.push(
+          `Status: "${currentTaskData?.status}" → "${updatePayload.status}"`
+        );
       }
 
       await updateDoc(taskUpdateDoc, updatePayload);
@@ -235,177 +250,234 @@ const AddTaskModal = ({
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black/75 flex justify-center items-center z-[1000]">
-      <div className={`relative bg-[#FFFFFF] sm:rounded-lg shadow w-xl ${addText?'md:w-xl':'md:w-5xl'} mt-0 sm:mt-10 flex flex-col rounded-t-3xl`}>
-        <div className="flex items-center justify-between p-2 border-b border-[#0000001A] rounded-t">
-          <h3 className="text-xl font-semibold text-gray-900">
+      <div
+        className={`relative bg-[#FFFFFF] sm:rounded-lg shadow w-xl ${
+          addText ? "md:w-xl" : "md:w-5xl"
+        } mt-0 sm:mt-10 flex flex-col rounded-t-3xl`}
+      >
+        <div className="flex items-center justify-left sm:justify-between p-2 sm:border-b sm:border-[#0000001A] rounded-t">
+          <h3
+            className={`text-xl font-semibold text-gray-900 ${
+              addText ? "sm:block":"sm:block hidden"
+            }`}
+          >
             {addText ? "Create Task" : "Update Task"}
           </h3>
           <button
             type="button"
-            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-lg w-8 h-8 flex justify-center items-center"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-lg w-8 h-8 justify-center items-center sm:block hidden"
             onClick={() => setOpenAddModal(false)}
           >
             ✖
           </button>
-        </div>
-        <div className="flex sm:flex-row  gap-4">
-        <div className="p-4 md:p-4 space-y-4 flex-grow">
-          <div>
-            <TextField
-              label="Task Title"
-              variant="outlined"
-              fullWidth
-              name="taskName"
-              value={taskDetail?.taskName || ""}
-              onChange={handleChangeInput}
-              size="small"
-            />
-          </div>
-
-          <div>
-            <TextField
-              label="Description"
-              variant="outlined"
-              fullWidth
-              size="small"
-              multiline
-              name="description"
-              value={taskDetail?.description || ""}
-              onChange={handleChangeInput}
-              rows={3}
-            />
-          </div>
-
-          <div className="flex sm:flex-row flex-col gap-4">
-            <div className="flex flex-col flex-1">
-              <label className="text-xs font-medium mb-1">Task Category</label>
-              <ToggleButtonGroup
-                color="primary"
-                exclusive
-                aria-label="Platform"
-                value={taskDetail?.category || ""}
-                onChange={(_, value) =>
-                  dispatch(
-                    setAccessData({
-                      type: "taskDetails",
-                      response: { ...taskDetail, category: value },
-                    })
-                  )
-                }
-                fullWidth
-              >
-                <ToggleButton size="small" value="W">
-                  Work
-                </ToggleButton>
-                <ToggleButton size="small" value="P">
-                  Personal
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </div>
-
-            <div className="flex flex-col flex-1">
-              <label className="text-xs font-medium mb-1">Due on</label>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  value={
-                    taskDetail?.dueDate ? dayjs(taskDetail?.dueDate) : null
-                  }
-                  onChange={handleChangeDate}
-                  slotProps={{ textField: { size: "small", fullWidth: true } }}
-                />
-              </LocalizationProvider>
-            </div>
-
-            <div className="flex flex-col flex-1">
-              <label className="text-xs font-medium mb-1">Task Status</label>
-              <FormControl fullWidth>
-                <Select
-                  labelId="task-status-label"
-                  id="task-status"
-                  size="small"
-                  name="status"
-                  value={taskDetail?.status || ""}
-                  onChange={handleChangeInput}
+          {!addText && (
+            <div className="sm:hidden block">
+              <FormControl>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                  onChange={(e) => {
+                    setSelectedOption(e?.target.value);
+                    console.log(e?.target?.value);
+                  }}
                 >
-                  <MenuItem value="T">TODO</MenuItem>
-                  <MenuItem value="P">In-Progress</MenuItem>
-                  <MenuItem value="C">Completed</MenuItem>
-                </Select>
+                  <FormControlLabel
+                    value="details"
+                    control={<Radio />}
+                    label="Details"
+                  />
+                  <FormControlLabel
+                    value="activity"
+                    control={<Radio />}
+                    label="Activity"
+                  />
+                </RadioGroup>
               </FormControl>
             </div>
-          </div>
-
-          <div className="flex flex-col flex-1">
-            <label className="text-xs font-medium mb-1">Attachment</label>
-            <Button
-              component="label"
-              variant="contained"
-              fullWidth
-              sx={{
-                backgroundColor: "#F1F1F15C",
-                color: "#000",
-                textTransform: "capitalize",
-                height: 50,
-              }}
-            >
-              Drop your files or upload
-              <VisuallyHiddenInput
-                type="file"
-                onChange={handleFileUpload}
-                multiple
-                accept="image/*, .pdf, .xlsx, .xls, .csv"
+          )}
+        </div>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div
+            className={`${
+              selectedOption === "details" ? "block" : "hidden sm:block"
+            } p-4 md:p-4 space-y-4 flex-grow`}
+          >
+            <div>
+              <TextField
+                label="Task Title"
+                variant="outlined"
+                fullWidth
+                name="taskName"
+                value={taskDetail?.taskName || ""}
+                onChange={handleChangeInput}
+                size="small"
               />
-            </Button>
+            </div>
 
-            {selectedFiles.length > 0 && (
-              <div className="mt-2 text-xs text-gray-700">
-                <strong>Uploaded Files:</strong>
-                <ul className="list-disc pl-4">
-                  {selectedFiles.map((file, index) => (
-                    <li key={index} className="flex items-center gap-2">
-                      {file.type.startsWith("image") && (
-                        <img
-                          src={URL.createObjectURL(file)}
-                          alt="preview"
-                          className="w-16 h-16 object-cover rounded"
-                        />
-                      )}
-                      <span>{file.name}</span>
-                      <button
-                        className="text-red-500 hover:underline ml-2"
-                        onClick={() => handleRemoveFile(index)}
-                      >
-                        Remove
-                      </button>
-                    </li>
-                  ))}
-                </ul>
+            <div>
+              <TextField
+                label="Description"
+                variant="outlined"
+                fullWidth
+                size="small"
+                multiline
+                name="description"
+                value={taskDetail?.description || ""}
+                onChange={handleChangeInput}
+                rows={3}
+              />
+            </div>
+
+            <div className="flex sm:flex-row flex-col gap-4">
+              <div className="flex flex-col flex-1">
+                <label className="text-xs font-medium mb-1">
+                  Task Category
+                </label>
+                <ToggleButtonGroup
+                  color="primary"
+                  exclusive
+                  aria-label="Platform"
+                  value={taskDetail?.category || ""}
+                  onChange={(_, value) =>
+                    dispatch(
+                      setAccessData({
+                        type: "taskDetails",
+                        response: { ...taskDetail, category: value },
+                      })
+                    )
+                  }
+                  fullWidth
+                >
+                  <ToggleButton size="small" value="W">
+                    Work
+                  </ToggleButton>
+                  <ToggleButton size="small" value="P">
+                    Personal
+                  </ToggleButton>
+                </ToggleButtonGroup>
               </div>
-            )}
-          </div>
-          </div>
-          <Divider orientation="vertical" variant="middle" flexItem />
-          {!addText && (
-            <div className="flex flex-col  my-2 mx-1 p-1">
-              <label className="text-sm font-medium mb-2 border-b border-[#00000021]">Activity</label>
-              <div className="bg-gray-100 p-4 rounded-2xl">
-                {logActivities.length > 0 ? (
-                  <ul className="space-y-2">
-                    {logActivities.map((log, index) => (
-                      <li key={index} className="text-xs text-gray-700">
-                        <strong>{dayjs(log?.timestamp).format("MMM D, YYYY h:mm A")}:</strong> {log?.description}
+
+              <div className="flex flex-col flex-1">
+                <label className="text-xs font-medium mb-1">Due on</label>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    value={
+                      taskDetail?.dueDate ? dayjs(taskDetail?.dueDate) : null
+                    }
+                    onChange={handleChangeDate}
+                    slotProps={{
+                      textField: { size: "small", fullWidth: true },
+                    }}
+                  />
+                </LocalizationProvider>
+              </div>
+
+              <div className="flex flex-col flex-1">
+                <label className="text-xs font-medium mb-1">Task Status</label>
+                <FormControl fullWidth>
+                  <Select
+                    labelId="task-status-label"
+                    id="task-status"
+                    size="small"
+                    name="status"
+                    value={taskDetail?.status || ""}
+                    onChange={handleChangeInput}
+                  >
+                    <MenuItem value="T">TODO</MenuItem>
+                    <MenuItem value="P">In-Progress</MenuItem>
+                    <MenuItem value="C">Completed</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+            </div>
+
+            <div className="flex flex-col flex-1">
+              <label className="text-xs font-medium mb-1">Attachment</label>
+              <Button
+                component="label"
+                variant="contained"
+                fullWidth
+                sx={{
+                  backgroundColor: "#F1F1F15C",
+                  color: "#000",
+                  textTransform: "capitalize",
+                  height: 50,
+                }}
+              >
+                Drop your files or upload
+                <input
+                  type="file"
+                  onChange={handleFileUpload}
+                  multiple
+                  accept="image/*, .pdf, .xlsx, .xls, .csv"
+                  hidden
+                />
+              </Button>
+
+              {selectedFiles.length > 0 && (
+                <div className="mt-2 text-xs text-gray-700">
+                  <strong>Uploaded Files:</strong>
+                  <ul className="list-disc pl-4">
+                    {selectedFiles.map((file, index) => (
+                      <li key={index} className="flex items-center gap-2">
+                        {file.type.startsWith("image") && (
+                          <img
+                            src={URL.createObjectURL(file)}
+                            alt="preview"
+                            className="w-16 h-16 object-cover rounded"
+                          />
+                        )}
+                        <span>{file.name}</span>
+                        <button
+                          className="text-red-500 hover:underline ml-2"
+                          onClick={() => handleRemoveFile(index)}
+                        >
+                          Remove
+                        </button>
                       </li>
                     ))}
                   </ul>
-                ) : (
-                  <p className="text-xs text-gray-500">No log activities found.</p>
-                )}
-              </div>
+                </div>
+              )}
             </div>
-          )}
-        
-        </div>
+          </div>
 
+          {/* Show Activity by default on larger screens */}
+          <div
+            className={`${
+              selectedOption === "activity" ? "block" : "hidden sm:block"
+            }`}
+          >
+            <Divider orientation="vertical" variant="middle" flexItem />
+            {!addText && (
+              <div className="flex flex-col my-2 mx-1 p-1">
+                <label className="text-sm font-medium mb-2 border-b border-[#00000021]">
+                  Activity
+                </label>
+                <div className="bg-gray-100 p-4 rounded-2xl">
+                  {logActivities.length > 0 ? (
+                    <ul className="space-y-2">
+                      {logActivities.map((log, index) => (
+                        <li key={index} className="text-xs text-gray-700">
+                          <strong>
+                            {dayjs(log?.timestamp).format("MMM D, YYYY h:mm A")}
+                            :
+                          </strong>{" "}
+                          {log?.description}
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p className="text-xs text-gray-500">
+                      No log activities found.
+                    </p>
+                  )}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
         <div className="flex justify-end gap-2 p-2 border-t border-gray-200 bg-[#F1F1F1] rounded-b">
           <button
             type="button"
